e8dacec5426f2f34f07c1137ed64501670a334ab
Adam Scott <ascott@slync.io>
2022-06-17 11:26:56 -0400

    Initial setup of routing with logs, folder, cors, and error handling

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,63 @@
-import express from "express";
+import express, { json, Express } from "express";
 import { Server } from "http";
+import errorHandler from "strong-error-handler";
+import morgan from "morgan";
+import cors from "cors";
 import { getDBConnection } from "./database";
+import { setupRoutes } from "./routes";
+import { isProdEnv } from "./utils/node";
+
+// Should be based on configuration file
+const port = 4040;
+
+// This is essentially the Apache combined log format with response time as well
+const prodLogFormat =
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" - :response-time ms';
+
+const setupMiddleware = (app: Express) => {
+    app.use(
+        errorHandler({
+            // Normally this would be based on configuration as well
+            debug: !isProdEnv(),
+            log: true,
+        })
+    );
+
+    // Normally this would be based on configuration as well
+    app.use(morgan(isProdEnv() ? prodLogFormat : "dev"));
+
+    // middleware for json body parsing
+    app.use(json({ limit: "5mb" }));
+
+    app.use(
+        cors({
+            allowedHeaders: ["Content-Type", "authorization"],
+            methods: ["GET", "POST"],
+            // This isn't how it usually would be setup, but for this context just assume that all
+            // requests will be coming from inside the same house!
+            origin: isProdEnv() ? "localhost" : "*",
+        })
+    );
+};
+
+const setupExpress = () => {
+    const app = express();
+
+    setupMiddleware(app);
+    setupRoutes(app);
+
+    const server = app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+        return server;
+    });
+    return server;
+};
 
 export const start = async (): Promise<Server> => {
     try {
-        const port = 4040;
-        const app = express();
         const db = await getDBConnection();
         await db.sync({ force: true });
-        app.get("/", (req, res) => {
-            res.send("Hello World!");
-        });
-
-        const server = app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`);
-            return server;
-        });
-        return server;
+        return setupExpress();
     } catch (err) {
         throw err;
     }
